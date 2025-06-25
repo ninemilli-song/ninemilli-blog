e3669dccaac2c9fa9291c944a28dfa35a3e751d6
ninemilli-song <ninemilli_song@163.com>
2025-06-25 12:47:31 +0800

    add ai-glossary.mdx

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: '主页' },
   { href: '/blog', title: '博客' },
   { href: '/tags', title: '标签' },
-  { href: '/projects', title: '花园' },
+  // { href: '/projects', title: '花园' },
   { href: '/about', title: '关于我' },
 ]
 
